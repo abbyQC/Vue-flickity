99e28f2467e8e2432c3889539e81401c87b82a17
Chang Qiu <qcabby1217@gmail.com>
2020-05-24 17:14:52 -0400

    double the images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,17 @@
             <img src="https://placeimg.com/640/480/any?4">
             <img src="https://placeimg.com/640/480/any?5">
         </carousel>
+
+        <h2>Another One</h2>
+
+        <carousel>
+            <img src="https://placeimg.com/640/480/any">
+            <img src="https://placeimg.com/640/480/any?2">
+            <img src="https://placeimg.com/640/480/any?3">
+            <img src="https://placeimg.com/640/480/any?4">
+            <img src="https://placeimg.com/640/480/any?5">
+        </carousel>
+
     </div>
     <script src="/js/app.js"></script>
 
